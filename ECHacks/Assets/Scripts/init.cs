4a4cf00eab43e56638550de29e82653391df82dc
Misha Larionov <mishalarionov2@gmail.com>
2016-11-05 12:35:18 -0400

    oh no

---
+++ ECHacks/Assets/Scripts/init.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class init : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		main.Init ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
