0fd8466f8903ca0b2b2058230e19270ab9bb9da9
Emily Goose <EmilyGoose@users.noreply.github.com>
2016-11-05 12:35:18 -0400

    oh no

---
+++ ECHacks/Assets/Scripts/binScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class binScript : MonoBehaviour, IGvrGazeResponder {
+
+	public Transform bin;
+	private main mainScript;
+	private GameObject trash;
+
+	// Update is called once per frame
+	void Update () {
+		trash = main.garbage;
+	}
+
+	#region IGvrGazeResponder implementation
+
+	/// Called when the user is looking on a GameObject with this script,
+	/// as long as it is set to an appropriate layer (see GvrGaze).
+	public void OnGazeEnter() {
+		
+	}
+
+	/// Called when the user stops looking on the GameObject, after OnGazeEnter
+	/// was already called.
+	public void OnGazeExit() {
+
+	}
+
+	/// Called when the viewer's trigger is used, between OnGazeEnter and OnGazeExit.
+	public void OnGazeTrigger() {
+
+	}
+
+	#endregion
+}
---
+++ ECHacks/Assets/Scripts/main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	public static GameObject garbage;
+	private static string objectToLoad;
+	private static string[] prefabs;
+
+	public static void findNewObject() {
+		objectToLoad = prefabs [Random.Range (0, prefabs.Length)];
+		garbage = Instantiate(Resources.Load(objectToLoad, typeof(GameObject))) as GameObject;
+	}
+
+	public static void Init() {
+		//Hardcoded list of instances
+		prefabs = new string[] { "Rtinfoil" };
+		main.findNewObject ();
+		garbage.transform.position = new Vector3 (0, 1, 2);
+	}
+}
