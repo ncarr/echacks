7848b52226a77b296472cca7f3c5529028f040b5
Misha Larionov <mishalarionov2@gmail.com>
2016-11-06 02:28:51 -0500

    Finalizing stuff - Nicholas

--- ECHacks/Assets/Scripts/main.cs
+++ ECHacks/Assets/Scripts/main.cs
@@ -4,18 +4,21 @@ using System.Collections;
 public class main : MonoBehaviour {
 
 	public static GameObject garbage;
+    public static bool destroy = false;
 	private static string objectToLoad;
 	private static string[] prefabs;
+	private static TextMesh gooey = Instantiate(Resources.Load("nameCanvas", typeof(TextMesh))) as TextMesh;
 
 	public static void findNewObject() {
 		objectToLoad = prefabs [Random.Range (0, prefabs.Length)];
 		garbage = Instantiate(Resources.Load(objectToLoad, typeof(GameObject))) as GameObject;
+		gooey.text = objectToLoad;
 	}
 
 	public static void Init() {
 		//Hardcoded list of instances
-		prefabs = new string[] { "Rtinfoil"}; //, "Cbanana" };
-		main.findNewObject ();
+		prefabs = new string[] {"Tinfoil", "Banana Peel" };
+		main.findNewObject();
 		moveDampScript.targetPosition = new Vector3 (0, 1, 2);
 		garbage.transform.position = new Vector3 (0, 1, 2);
 	}
--- ECHacks/Assets/Scripts/moveDampScript.cs
+++ ECHacks/Assets/Scripts/moveDampScript.cs
@@ -15,5 +15,11 @@ public class moveDampScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		transform.position = Vector3.SmoothDamp(transformersRevengeOfTheFallen.position, targetPosition, ref velocity, 1);
+        if (main.destroy && velocity != Vector3.zero) {
+            Destroy(main.garbage);
+            main.findNewObject();
+            targetPosition = new Vector3(0, 1, 2);
+            main.garbage.transform.position = new Vector3(0, 1, 2);
+        }
 	}
 }
